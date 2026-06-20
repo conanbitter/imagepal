ec0fb6904262930aed0cd49ed54d87855fa09813
Conan Bitter <conan.bitter@gmail.com>
2026-06-20 14:35:44 +0300

    Introduce subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
+use console::style;
 use image::ImageReader;
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use std::{path::PathBuf, str::FromStr, time::Duration};
@@ -14,27 +15,41 @@ mod palgen;
 mod report;
 
 #[derive(Parser, Debug)]
-struct Args {
+#[command(
+    name = "imagepal",
+    version = "0.1",
+    about = "A palette generator for groups of images"
+)]
+struct AppArgs {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Generate(GenArgs),
+}
+
+#[derive(Args, Debug)]
+struct GenArgs {
     #[arg(required = true)]
     files: Vec<PathBuf>,
 }
 
-fn main() -> anyhow::Result<()> {
-    println!("= IMAGEPAL v0.1 =\n");
+fn command_generate(args: GenArgs) -> anyhow::Result<()> {
+    println!("{} Generate palette\n", style("│").green());
 
-    let args = Args::parse_from(wild::args());
+    let multi = MultiProgress::new();
 
-    let m = MultiProgress::new();
-
-    let title_spinner = m.add(ProgressBar::new_spinner());
+    let title_spinner = multi.add(ProgressBar::new_spinner());
     title_spinner.set_style(ProgressStyle::with_template("{spinner:.green} {msg}...\n\n").unwrap());
+    title_spinner.enable_steady_tick(Duration::from_millis(100));
 
     // LOAD IMAGES
 
-    let load_status = LoadStatus::new(&m, args.files.len());
+    let load_status = LoadStatus::new(&multi, args.files.len());
 
     title_spinner.set_message("Loading images");
-    title_spinner.enable_steady_tick(Duration::from_millis(100));
 
     let mut cube = ColorCube::new();
 
@@ -65,15 +80,33 @@ fn main() -> anyhow::Result<()> {
 
     let mut palgen = PalGen::new(256, cube)?;
 
-    let mut calc_status = CalcStatus::new(&m, 5, 2000);
+    let mut calc_status = CalcStatus::new(&multi, 5, 2000);
 
     let result_palette = palgen.run(&mut calc_status, 5, 2000)?;
 
     calc_status.finish();
-    title_spinner.finish_and_clear();
 
     save_palette(&result_palette, PathBuf::from_str("result.ipal")?)?;
+
+    title_spinner.finish_and_clear();
     println!("Done!");
+
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    println!(
+        "{}\n{} {}",
+        style("╭──────────").green(),
+        style("│").green(),
+        style("IMAGEPAL").bold()
+    );
+
+    let args = AppArgs::parse_from(wild::args());
+
+    match args.command {
+        Commands::Generate(gen_args) => command_generate(gen_args)?,
+    }
 
     Ok(())
 }
